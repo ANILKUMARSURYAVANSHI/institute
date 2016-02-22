3085a771a683f6437b26df0353849329ef607e28
Ravinder-kumar95 <ravinderkumar95@outlook.com>
2016-02-22 20:56:06 +0530

    Template

---
+++ registration.php
@@ -0,0 +1,15 @@
+<html>
+<head><title>Registration page</title>
+	</head>
+	<body>
+		<div id="d_registration">
+			<form method=post action="#">
+				<input type="text" class="form-control" name="firstname" placeholder="First Name">
+
+			</form>
+
+		</div>
+
+
+	</body>
+</html>
